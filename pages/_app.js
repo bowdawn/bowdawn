756e5fd294ccc7bdbf2fc4a59bc7a91fcb9e3852
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-05-28 20:27:53 +0900

    delete redux

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,13 @@ import { Fragment } from 'react';
 import App, { Container } from 'next/app';
 import Head from 'next/head';
 import { Provider } from 'react-redux';
-import withRedux from 'next-redux-wrapper';
-import withReduxSaga from 'next-redux-saga';
-import createStore from '../redux/store';
+
 import Layout from '../components/Layout';
 
 import '../assets/self-styles.less';
 
 
-class NextApp extends App {
+export default class NextApp extends App {
 
   static async getInitialProps({ Component, ctx }) {
     let pageProps = {};
@@ -45,4 +43,3 @@ class NextApp extends App {
   }
 }
 
-export default withRedux(createStore)(withReduxSaga({ async: true })(NextApp));
