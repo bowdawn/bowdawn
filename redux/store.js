c70fd6b1f4af961ac6a4179b8f038d1350d2d20f
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-05-28 19:56:48 +0900

    fix: delete middle ware

--- redux/store.js
+++ redux/store.js
@@ -2,13 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from './reducers/index';
 import rootSaga from './sagas/index';
-import userMiddleware from '../middlewares/client/user';
+
 
 const sagaMiddleware = createSagaMiddleware();
 
 const bindMiddleware = (middleware) => {
   // add route middleware
-  middleware.push(userMiddleware);
+
   if (process.env.NODE_ENV !== 'production') {
     const { composeWithDevTools } = require('redux-devtools-extension');
     // development use logger
@@ -19,7 +19,7 @@ const bindMiddleware = (middleware) => {
   return applyMiddleware(...middleware);
 };
 
-function configureStore (initialState) {
+function configureStore(initialState) {
   const store = createStore(
     rootReducer,
     initialState,
