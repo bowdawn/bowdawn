3d829b82ac82d397d11d5e16cf2f75f304d6f205
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-05-29 20:25:42 +0900

    delete core

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Button } from 'antd';
 export default class Welcome extends Component {
   render() {
     return (<div>
-      <div>Delete Redux
+      <div>Delete Core
         
       </div>
       <Button type="primary">bowdawn</Button>
