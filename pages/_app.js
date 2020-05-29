18a8069e6bac9a7499e6b7a2f73786b97ab94c48
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-05-29 21:35:10 +0900

    add ant icons library

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,17 @@ import Head from 'next/head';
 
 import '../assets/self-styles.less';
 
+import { Layout, Menu } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+
+const { Header, Sider, Content } = Layout;
+
 
 export default class NextApp extends App {
 
@@ -17,6 +28,17 @@ export default class NextApp extends App {
 
     return { pageProps };
   }
+
+
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
 
   render() {
     const { Component, pageProps, router } = this.props;
@@ -32,7 +54,43 @@ export default class NextApp extends App {
         <Container>
 
 
-          <Component {...pageProps} router={router} />
+          <Layout>
+            <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+              <div className="logo" />
+              <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                <Menu.Item key="1" icon={<UserOutlined />}>
+                  nav 1
+            </Menu.Item>
+                <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                  nav 2
+            </Menu.Item>
+                <Menu.Item key="3" icon={<UploadOutlined />}>
+                  nav 3
+            </Menu.Item>
+              </Menu>
+            </Sider>
+            <Layout className="site-layout">
+              <Header className="site-layout-background" style={{ padding: 0 }}>
+                {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                  className: 'trigger',
+                  onClick: this.toggle,
+                })}
+              </Header>
+              <Content
+                className="site-layout-background"
+                style={{
+                  margin: '24px 16px',
+                  padding: 24,
+                  minHeight: 280,
+                }}
+              >
+                <Component {...pageProps} router={router} />
+              </Content>
+            </Layout>
+          </Layout>
+
+
+
 
 
         </Container>
