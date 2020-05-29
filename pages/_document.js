88c33c323ad2c4332fa24c502e6b26a38a07e250
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-05-29 14:25:47 +0900

    delete files / attempt firebase connection

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@
 // ./pages/_document.js
 import Document, { Head, Main, NextScript } from 'next/document';
 
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -22,6 +23,30 @@ export default class MyDocument extends Document {
           `}</style>
         </Head>
         <body>
+          <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.14.5/firebase-app.js"></script>
+
+          <!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.14.5/firebase-analytics.js"></script>
+
+          <script>
+  // Your web app's Firebase configuration
+            var firebaseConfig = {
+              apiKey: "AIzaSyDjl80n7jhBGTjElcxB4pbq3L5X2CCNDbc",
+    authDomain: "bowdawn-jade.firebaseapp.com",
+    databaseURL: "https://bowdawn-jade.firebaseio.com",
+    projectId: "bowdawn-jade",
+    storageBucket: "bowdawn-jade.appspot.com",
+    messagingSenderId: "224887390799",
+    appId: "1:224887390799:web:ed893c1fa375196a46e03a",
+    measurementId: "G-CFK77J8VVQ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+
           <Main />
           <NextScript />
         </body>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import { Component } from 'react';
 import { Button } from 'antd';
 
+
+import firebase from "../firebase"
+
+
+
 export default class Welcome extends Component {
   render() {
+    firebase.firestore().collection("people").add({ id: 1, name: "bowdawn" });
     return (<div>
       <div>Hello World</div>
       <Button type="primary">Hello World</Button>
