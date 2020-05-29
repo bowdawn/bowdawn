42c36d4db2cfcd46eb6c131968492df6675724c3
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-05-29 15:22:21 +0900

    fix: delete firebase

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,16 @@ import { Component } from 'react';
 import { Button } from 'antd';
 
 
-import firebase from "../firebase"
+
 
 
 
 export default class Welcome extends Component {
   render() {
-    firebase.firestore().collection("people").add({ id: 1, name: "bowdawn" });
+
     return (<div>
       <div>Hello World</div>
-      <Button type="primary">Hello World</Button>
+      <Button type="primary">bowdawn</Button>
     </div>);
   }
 }
