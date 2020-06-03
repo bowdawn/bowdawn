7d58e74fe6a1ca2d108435a938a21d8fa423c905
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-06-03 12:25:06 +0900

    update with exposed firebase

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import { Component } from 'react';
 import { Button } from 'antd';
+import useSWR from 'swr';
+
+const fetcher = async (...args) => {
+  const res = await fetch(...args);
+  console.log(res);
+  return res.json();
+};
 
 export default class Welcome extends Component {
   render() {
     return (<div>
       <div>
-        Hello Vercel Firebase! updated react / react-dom
+        Hello Vercel Firebase! updated react / react-dom, import firebase adming and swr
       </div>
       <Button type="primary" >bowdawn</Button>
     </div>);
