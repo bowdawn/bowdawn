0692a9bfa2c2320137ce7bc499e80b284921f85c
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-06-03 13:55:58 +0900

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,73 +1,40 @@
-
-const withLess = require('@zeit/next-less');
-const lessToJS = require('less-vars-to-js');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const fs = require('fs');
-const path = require('path');
+/* eslint-disable */
+const withLess = require('@zeit/next-less')
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
+const path = require('path')
 
 // Where your antd-custom.less file lives
 const themeVariables = lessToJS(
-  fs.readFileSync(
-    path.resolve(__dirname, './assets/antd-custom.less'),
-    'utf8',
-  )
-);
-
-const isDev = process.env.NODE_ENV !== 'production';
-
-// fix antd bug in dev development
-const devAntd = '@import "~antd/dist/antd.less";\n';
-const stylesData = fs.readFileSync(
-  path.resolve(__dirname, './assets/_styles.less'),
-  'utf-8'
-);
-fs.writeFileSync(
-  path.resolve(__dirname, './assets/self-styles.less'),
-  isDev ? `${devAntd}${stylesData}` : stylesData,
-  'utf-8'
-);
-
-// fix: prevents error when .css files are required by node
-if (typeof require !== 'undefined') {
-  require.extensions['.less'] = (file) => { }
-}
+  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+)
 
 module.exports = withLess({
-  target: 'serverless',
   lessLoaderOptions: {
     javascriptEnabled: true,
-    modifyVars: themeVariables,
-    localIdentName: '[local]___[hash:base64:5]',
+    modifyVars: themeVariables, // make your antd custom effective
   },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
-    if (!dev) {
-      config.plugins.push(
-        ...[
-          new BundleAnalyzerPlugin({
-            analyzerMode: 'disabled',
-            // For all options see https://github.com/th0r/webpack-bundle-analyzer#as-plugin
-            generateStatsFile: true,
-            // Will be available at `.next/stats.json`
-            statsFilename: 'stats.json'
-          }),
-        ]);
-      config.devtool = 'source-map';
-    } else {
-      config.module.rules.push({
-        test: /\.js$/,
-        enforce: 'pre',
-        include: [
-          path.resolve('components'),
-          path.resolve('pages'),
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/
+      const origExternals = [...config.externals]
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback()
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback)
+          } else {
+            callback()
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ]
 
-          path.resolve('constants'),
-
-        ],
-
-      });
-      config.devtool = 'cheap-module-inline-source-map';
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      })
     }
-    return config;
+    return config
   },
-
-});
+})
