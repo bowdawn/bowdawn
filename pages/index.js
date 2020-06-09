5ba6198fd6d845b1141ff7d4d7565331fc65a8b5
Bogdan Oleinikov <bowdawn@jinjoosoft.com>
2020-06-09 18:05:55 +0900

    ant design upload images

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,80 @@
 import React, { useState } from "react";
-
 import { storage } from "@lib/base";
-
+import { Upload, Modal, message } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
 export default function ReactFirebaseFileUpload() {
-  const [image, setImage] = useState(null);
-  const [url, setUrl] = useState("");
-  const [progress, setProgress] = useState(0);
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [previewImage, setPreviewImage] = useState("");
+  const [previewTitle, setPreviewTitle] = useState("");
+  const [fileList, setFileList] = useState([]);
+  const handlePreview = async file => {
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewVisible(true);
+    setPreviewTitle(file.name || file.url.substring(file.url.lastIndexOf('/') + 1));
+  };
+  const onChange = async (info) => {
 
-  const handleChange = e => {
-    if (e.target.files[0]) {
-      setImage(e.target.files[0]);
+    if (info.file.status === 'uploading') {
+      const uploadTask = storage.ref(`images/${info.file.name}`).put(info.file.originFileObj);
+      uploadTask.on(
+        "state_changed",
+        () => { },
+        error => {
+          console.log(error);
+        },
+        () => {
+          storage
+            .ref("images")
+            .child(info.file.name)
+            .getDownloadURL()
+            .then(url => {
+              setUrl(url);
+            });
+        }
+      );
+      setFileList(info.fileList);
+    }
+    if (info.file.status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
     }
   };
-
-  const handleUpload = () => {
-    const uploadTask = storage.ref(`images/${image.name}`).put(image);
-    uploadTask.on(
-      "state_changed",
-      snapshot => {
-        const progress = Math.round(
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        );
-        setProgress(progress);
-      },
-      error => {
-        console.log(error);
-      },
-      () => {
-        storage
-          .ref("images")
-          .child(image.name)
-          .getDownloadURL()
-          .then(url => {
-            setUrl(url);
-          });
-      }
-    );
-  };
-
-  console.log("image: ", image);
-
-  return (
+  const uploadButton = (
     <div>
-      <progress value={progress} max="100" />
-      <br />
-      <br />
-      <input type="file" onChange={handleChange} />
-      <button onClick={handleUpload}>Upload</button>
-      <br />
-      {url}
-      <br />
-      <img src={url || "http://via.placeholder.com/300"} alt="firebase-image" />
+      <PlusOutlined />
+      <div className="ant-upload-text">Upload</div>
     </div>
   );
+  return (
+    <div>
+      <Upload
+        listType="picture-card"
+        fileList={fileList}
+        onPreview={handlePreview}
+        onChange={(info) => onChange(info)}
+      >
+        {fileList.length >= 8 ? null : uploadButton}
+      </Upload>
+      <Modal
+        visible={previewVisible}
+        title={previewTitle}
+        footer={null}
+        onCancel={() => setPreviewVisible(false)}
+      >
+        <img alt="example" style={{ width: '100%' }} src={previewImage} />
+      </Modal>
+    </div >
+  );
 };
+function getBase64(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+}
